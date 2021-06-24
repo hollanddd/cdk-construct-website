96f162589f4efffcdada2d36cf9608f8a35ff88a
Darren <me@darrenholland.com>
2021-06-24 15:34:38 -0500

    chore(deps): bump version

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,7 @@ const { AwsCdkConstructLibrary } = require('projen');
 const project = new AwsCdkConstructLibrary({
   author: 'Darren',
   authorAddress: 'me@darrenholland.com',
-  cdkVersion: '1.109.0',
+  cdkVersion: '1.110.0',
   defaultReleaseBranch: 'main',
   jsiiFqn: 'projen.AwsCdkConstructLibrary',
   name: 'cdk-construct-website',
